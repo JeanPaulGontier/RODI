6f15d615d1d40beeee94216cd5d72ed8c6c64b23
Jean-Paul Gontier <polo@pololand.com>
2023-06-21 08:30:22 +0200

    20230621

---
+++ web/DesktopModules/AIS/Admin Contacts Lists/Settings.ascx.cs
@@ -0,0 +1,71 @@
+﻿using DotNetNuke.Entities.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+/** 
+
+catégories par défaut pour clubs
+ 
+Actions
+Bulletin
+Calendrier
+Conférence
+Divers
+Galeries de photos
+Lettre du président
+in memoriam
+
+
+catégories par défaut pour district
+
+Actualités
+Actions
+Galeries de photos
+Lettre du gouverneur
+in memoriam
+
+**/
+
+public partial class DesktopModules_AIS_Admin_Contacts_Lists_Settings : ModuleSettingsBase
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+            return;
+
+        ModuleController objModules = new ModuleController();
+        string valueRBL = "" + Settings["mode"];
+        foreach (ListItem li in mode.Items)
+        {
+            if (li.Value == valueRBL)
+                li.Selected = true;
+        }
+
+        categories.Text = "" + Settings["categories"];
+
+    }
+
+    public override void UpdateSettings()
+    {
+        base.UpdateSettings();
+
+        DotNetNuke.Entities.Modules.ModuleController objModules3 = new DotNetNuke.Entities.Modules.ModuleController();
+        
+        objModules3.UpdateModuleSetting(ModuleId, "mode", mode.SelectedValue);
+        objModules3.UpdateModuleSetting(ModuleId, "categories", categories.Text);
+    }
+
+    protected void btreset_Click(object sender, EventArgs e)
+    {
+        if(mode.SelectedValue=="district")
+            categories.Text = "Actualités\r\nActions\r\nGaleries de photos\r\nLettre du gouverneur\r\nin memoriam";
+        else
+            categories.Text = "Actions\r\nBulletin\r\nCalendrier\r\nConférence\r\nDivers\r\nGaleries de photos\r\nLettre du président\r\nin memoriam";
+    }
+
+}
---
+++ web/DesktopModules/AIS/Admin Contacts Lists/app.js
@@ -0,0 +1,40 @@
+﻿function InitApp(appid, MODULEID, CONTEXT, CRIC, MODE, EDITABLE) {
+
+
+    const routes = [
+        { path: '/', component: News, props: true },
+        { path: '/edit/:id', component: NewsEdit, props: true },
+        { path: '/view/:id', component: NewsView, props: true }
+    ];
+
+    const router = VueRouter.createRouter({
+        history: VueRouter.createWebHashHistory(),
+        routes, // short for `routes: routes`,
+        //base: window.location.pathname,
+    });
+
+    const app = Vue.createApp({
+        data() {
+            return {
+                moduleid: MODULEID,
+                context: {
+                    context: CONTEXT,
+                    cric: CRIC,
+                    mode: MODE
+                },
+                editable: EDITABLE
+            }
+        },
+        components: {
+           
+        },
+        methods: {
+           
+        },
+        mounted() {
+          
+        }
+    });
+    app.use(router);
+    app.mount('#' + appid);
+}
